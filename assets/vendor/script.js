39f8638b339cc9a6e2b2362a35054548d9509f02
dhanraj shetty <dhanrajshetty1997@gmail.com>
2022-03-31 19:39:44 +0530

    added clear error message on cancel button

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -20,10 +20,14 @@ $(document).ready(function () {
             $('#fname-msg').show();
             $('#fname-msg').text('**First Name Field cant be empty!!**');
             return false;
+        } else if ((fname.val().length < 3) || (fname.val().length > 12)) {
+            $('#fname-msg').show();
+            $('#fname-msg').text('**First Name should not be less than 3 or greater than 12!**');
+            return false;
         } else if (!characterReg.test(fname.val())) {
             $('#fname-msg').show();
             $('#fname-msg').text('**Please Enter a valid name!**');
-            return false
+            return false;
         }
 
         if (lname.val() == "" || lname.val() == null) {
@@ -34,11 +38,13 @@ $(document).ready(function () {
             $('#lname-msg').show();
             $('#lname-msg').text('**Please Enter a valid Last name!**');
             return false;
+        } else if ((lname.val().length < 3) || (lname.val().length > 12)) {
+            $('#fname-msg').show();
+            $('#fname-msg').text('**last Name should not be less than 3 or greater than 12!**');
+            return false;
         }
 
-        if (gender_radio_btn.is(':checked')) {
-            $('#gender-msg').hide();
-        } else {
+        if (!(gender_radio_btn.is(':checked'))) {
             $('#gender-msg').show();
             $('#gender-msg').text('**Please select your gender!!**')
             return false;
@@ -51,6 +57,10 @@ $(document).ready(function () {
         } else if (!addressReg.test(address.val())) {
             $('#address-msg').show();
             $('#address-msg').text('**Plese enter valid Address!!**');
+            return false;
+        } else if (address.val().length < 10) {
+            $('#address-msg').show();
+            $('#address-msg').text('**PLease enter Full Address!**');
             return false;
         }
 
@@ -71,6 +81,7 @@ $(document).ready(function () {
         $('#address').val('');
         $(gender_radio_btn).prop('checked', false)
         $(terms).prop('checked', false);
+        $('.error-message').hide();
         e.preventDefault();
     })
 })
