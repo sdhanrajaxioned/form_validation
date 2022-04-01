2d0c7514dd910740b53ed1400fe1364a1012dac6
dhanraj shetty <dhanrajshetty1997@gmail.com>
2022-04-01 14:09:47 +0530

    added seperate function for validations

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -1,76 +1,103 @@
 $(document).ready(function () {
     var fname = $('#fname');
     var lname = $('#lname');
+    var email = $('#email');
     var gender_radio_btn = $('input:radio[name="gender"]');
     var address = $('#address');
     var terms = $('input:checkbox[name="terms"]');
-    console.log(terms.is(':checked'));
+    var emailReg = /^[\w]{1,}[\w.+-]{0,}@[\w-]{1,}([.][a-zA-Z]{2,3}|[.][\w-]{2,3}[.][a-zA-Z]{2,3})$/;
     var characterReg = /^[A-Za-z]+$/;
     var addressReg = /^[a-zA-Z0-9-\/] ?([a-zA-Z0-9-\/]|[a-zA-Z0-9-\/] )*[a-zA-Z0-9-\/]$/;
+    var fname_error = $('#fname-msg')
+    var lname_error = $('#lname-msg')
 
-    $('#fname-msg').hide()
-    $('#lname-msg').hide()
-    $('#gender-msg').hide()
-    $('#address-msg').hide()
-    $('#terms-msg').hide()
+    // function empty_field() {
+    //     $('form').each(function() {
+    //        var inputs = ($(this).find(':input'));
+    //        console.log(inputs)
+    //        inputs.each(function() {
+    //           if($(this).val() == ""){
+    //               $('.error-message').text("**This field is required!!**")
+    //           } else {
+    //               $('.error-message').text('')
+    //           }
+    //        })
+    //     })
+    // }
 
-
-    function validateForm() {
-        if (fname.val() == '' || fname.val() === null) {
-            $('#fname-msg').show();
-            $('#fname-msg').text('**First Name Field cant be empty!!**');
+    function validateName(name,error,regex) {
+        if(name.val() == '' || name.val() === null) {
+            $(error).text(`**${$(name).attr('name')} name field cant be empty!!**`);
             return false;
-        } else if ((fname.val().length < 3) || (fname.val().length > 12)) {
-            $('#fname-msg').show();
-            $('#fname-msg').text('**First Name should not be less than 3 or greater than 12!**');
+        } else if(!regex.test(name.val())) {
+            $(error).text('**Please Enter a valid name!**');
             return false;
-        } else if (!characterReg.test(fname.val())) {
-            $('#fname-msg').show();
-            $('#fname-msg').text('**Please Enter a valid name!**');
+        } else if($(error) != '') {
+            $(error).hide()
+        }
+    }
+
+    function remove_error_radio() {
+        $(gender_radio_btn).change(function() {
+            $('#gender-msg').hide();
+        })
+    }
+
+    function validateGender() {
+        if(!(gender_radio_btn.is(':checked'))) {
+            $('#gender-msg').text('**Please select your gender**')
             return false;
         }
+    }
 
-        if (lname.val() == "" || lname.val() == null) {
-            $('#lname-msg').show();
-            $('#lname-msg').text('**Last Name Field cant be empty!!**');
+    function validateAddress() {
+        if(address.val() == "" || address.val() === null) {
+        $('#address-msg').text('**Address field cant be empty!!**')
+        return false;
+        } else if(!addressReg.test(address.val())) {
+            $('#address-msg').text('**Plese enter valid Address**');
             return false;
-        } else if (!characterReg.test(lname.val())) {
-            $('#lname-msg').show();
-            $('#lname-msg').text('**Please Enter a valid Last name!**');
-            return false;
-        } else if ((lname.val().length < 3) || (lname.val().length > 12)) {
-            $('#fname-msg').show();
-            $('#fname-msg').text('**last Name should not be less than 3 or greater than 12!**');
-            return false;
-        }
-
-        if (!(gender_radio_btn.is(':checked'))) {
-            $('#gender-msg').show();
-            $('#gender-msg').text('**Please select your gender!!**')
-            return false;
-        }
-
-        if (address.val() == "" || address.val() === null) {
-            $('#address-msg').show();
-            $('#address-msg').text('**Address field cant be empty!!**')
-            return false;
-        } else if (!addressReg.test(address.val())) {
-            $('#address-msg').show();
-            $('#address-msg').text('**Plese enter valid Address!!**');
-            return false;
-        } else if (address.val().length < 10) {
-            $('#address-msg').show();
+        } else if(address.val().length < 10) {
             $('#address-msg').text('**PLease enter Full Address!**');
             return false;
+        } else if('#address-msg' != '') {
+            $('#address-msg').hide();
         }
+    }
 
+    function validateCheckbox() {
         if (!(terms.is(':checked'))) {
             $('#terms-msg').show();
             $('#terms-msg').text('**Please accept terms and condition!!**');
             return false;
-        }
+        } 
+    }
 
-        $('main').html('<h1>Form Sent Successfully!!</h1>');
+    function remove_terms() {
+        $(terms).change(function() {
+            $('#terms-msg').toggle()
+        })
+    }
+    
+    function validateForm() {
+        validateName(fname,fname_error,characterReg);
+        validateName(lname,lname_error,characterReg);
+        validateAddress()
+        validateGender();
+        remove_error_radio();
+        validateCheckbox();
+        remove_terms();
+
+        if(email.val() == '' || email.val() === null) {
+            $('#email-msg').text('**Email Field cant be empty!!**');
+            return false
+        } else if(!emailReg.test(email.val())) {
+            $('#email-msg').text('**Please enter valid email**');
+            return false
+        } else if($('#email-msg') != '') {
+            $('#email-msg').hide();
+        }
+        alert('form submitted Successfully')
         return true;
     }
 
